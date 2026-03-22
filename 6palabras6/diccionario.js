80082ebdb1a8fe080403dd660c75405d159e2005
poliestireno <59146512+poliestireno@users.noreply.github.com>
2026-03-22 19:53:01 +0100

    Add initial dictionary of words in diccionario.js

---
+++ 6palabras6/diccionario.js
@@ -0,0 +1,3 @@
+// Añade aquí todas las palabras que quieras (en mayúsculas)
+const DICCIONARIO = ["CASA", "PERRO", "GATO", "SOL", "MESA", "COMER", "AMIGO", "ESPAÑA", "JUEGO", "PALABRA", "BARCO", "CIELO"]; 
+// Nota: Para un diccionario real de 80.000 palabras, el archivo pesaría unos 2MB.
